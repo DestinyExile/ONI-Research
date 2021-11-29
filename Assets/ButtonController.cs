970b6006b9dbdb36e161cd0b61e4bf6876cdcfd8
Marcus Chang <marxalex7@gmail.com>
2021-11-29 15:47:55 -0600

    Node selecting and highlighting

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
+
+public class ButtonController : MonoBehaviour
+{
+    [SerializeField] GameObject NodeHighlight;
+    GameObject button;
+    bool selected = false;
+    bool hovered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selected)
+        {
+            NodeHighlight.SetActive(true);
+        }
+        else if (hovered)
+        {
+            NodeHighlight.SetActive(true);
+        }
+        else
+        {
+            NodeHighlight.SetActive(false);
+        }
+    }
+
+    public void ToggleSelection()
+    {
+        selected = !selected;
+    }
+
+    public void ToggleHover()
+    {
+        hovered = !hovered;
+    }
+}
